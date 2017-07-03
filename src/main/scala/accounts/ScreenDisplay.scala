6ff1112c938cdb100e91560aa8e83beee95c563f
administrator <administrator@VE-MAC-395.local>
2017-07-04 00:11:50 +0100

    removed unused code

--- src/main/scala/accounts/ScreenDisplay.scala
+++ src/main/scala/accounts/ScreenDisplay.scala
@@ -1,6 +1,4 @@
 package accounts
-
-import java.util.Scanner
 class ScreenDisplay(reader: ContentReader, path: String) {
 
   lazy val transactions = reader.getFileContent(path)
@@ -19,39 +17,4 @@ class ScreenDisplay(reader: ContentReader, path: String) {
     val records = TransactionSystem.rollingTimeWindowCalc(transactions, day)
     records.foreach(p => println(s"Day: ${p.day} - Accountid:${p.accountId} - Maximum: ${p.maximum} - Average: ${p.average} - AA Total:${p.aa} - CC Total:${p.cc} - FF Total:${p.ff}"))
   }
-
-/*
-  def choose = {
-    println("Choose from these choices")
-    println("-------------------------\n")
-    println("1 - Calculate the total ")
-    println("2 - Calculate the average")
-    println("3 - Calculate statistics for each account")
-    println("4 - Quit")
-    println()
-  }
-
-  def printAll(): Unit = {
-    val scanner = new Scanner(System.in)
-    val input = scanner.nextInt()
-
-    do{
-
-    }while(input)
-      input match {
-        case 1 => printTotal
-        case 2 => printAvg
-        case 3 => {
-          println("Please choose day for rolling time")
-          println()
-          val sc = new Scanner(System.in)
-          val day = sc.nextInt()
-          printStat(day)
-        }
-        case 4 => System.exit(0)
-        case _ => println("Choice no valid")
-      }
-  }
-*/
-
 }
